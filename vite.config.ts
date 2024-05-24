55be18ce2b75480581c438c20c82550eb5644c2d
SKairinos <stefan.kairinos@ocado.com>
2024-05-24 08:52:19 +0000

    initial

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vitest/config"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests",
+    mockReset: true,
+    coverage: {
+      enabled: true,
+      provider: "istanbul",
+      reporter: ["html", "cobertura"],
+    },
+  },
+})
