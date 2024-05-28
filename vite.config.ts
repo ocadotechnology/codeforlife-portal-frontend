75a1887008956238634fea97e12d9ca23f142baa
SKairinos <stefan.kairinos@ocado.com>
2024-05-28 11:22:03 +0000

    only assert coverage in verbose command

--- vite.config.ts
+++ vite.config.ts
@@ -16,12 +16,6 @@ export default defineConfig({
       enabled: true,
       provider: "istanbul",
       reporter: ["html", "cobertura"],
-      thresholds: {
-        lines: 90,
-        functions: 90,
-        branches: 90,
-        statements: 90,
-      },
     },
   },
 })
