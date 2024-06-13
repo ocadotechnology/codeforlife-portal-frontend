958d6ecf1c3489276f724805b6a1917331db3256
SKairinos <stefan.kairinos@ocado.com>
2024-06-13 15:09:08 +0000

    auto log in student

--- src/pages/login/studentForms/Class.tsx
+++ src/pages/login/studentForms/Class.tsx
@@ -1,11 +1,14 @@
 import { ChevronRight as ChevronRightIcon } from "@mui/icons-material"
 import { Stack, Typography } from "@mui/material"
-import type { FC } from "react"
+import { useEffect, type FC } from "react"
 import { generatePath } from "react-router-dom"
+import * as yup from "yup"
 
 import * as form from "codeforlife/components/form"
-import { useNavigate } from "codeforlife/hooks"
+import { useNavigate, useSearchParamEntries } from "codeforlife/hooks"
+import { tryValidateSync } from "codeforlife/utils/schema"
 
+import { useAutoLoginAsStudentMutation } from "../../../api/sso"
 import { classIdSchema } from "../../../app/schemas"
 import { paths } from "../../../router"
 import BaseForm from "../BaseForm"
@@ -13,34 +16,77 @@ import BaseForm from "../BaseForm"
 export interface ClassProps {}
 
 const Class: FC<ClassProps> = () => {
+  const [autoLoginAsStudent] = useAutoLoginAsStudentMutation()
   const navigate = useNavigate()
 
+  const searchParams = tryValidateSync(
+    useSearchParamEntries(),
+    yup.object({
+      userId: yup.number().required(),
+      agp: yup.string().required(),
+    }),
+  )
+
+  useEffect(() => {
+    if (searchParams) {
+      autoLoginAsStudent({
+        user_id: searchParams.userId,
+        auto_gen_password: searchParams.agp,
+      })
+        .unwrap()
+        .then(() => {
+          navigate(paths.student.dashboard.dependent._)
+        })
+        .catch(() => {
+          navigate(".", {
+            replace: true,
+            state: {
+              notifications: [
+                {
+                  props: {
+                    error: true,
+                    children:
+                      "Failed to automatically log in student. Please log" +
+                      "in manually.",
+                  },
+                },
+              ],
+            },
+          })
+        })
+    }
+  }, [searchParams, autoLoginAsStudent, navigate])
+
   return (
-    <BaseForm
-      themedBoxProps={{ userType: "student" }}
-      header="Welcome"
-      subheader="Please enter your class code."
-      initialValues={{ classId: "" }}
-      onSubmit={({ classId }) => {
-        navigate(generatePath(paths.login.student.class._, { classId }))
-      }}
-    >
-      <form.TextField
-        name="classId"
-        placeholder="Access code"
-        helperText="Enter your access code"
-        schema={classIdSchema}
-        required
-      />
-      <Typography variant="body2" fontWeight="bold">
-        Forgotten your login details? Please check with your teacher.
-      </Typography>
-      <Stack alignItems="end">
-        <form.SubmitButton endIcon={<ChevronRightIcon />}>
-          Next
-        </form.SubmitButton>
-      </Stack>
-    </BaseForm>
+    <>
+      {!searchParams && (
+        <BaseForm
+          themedBoxProps={{ userType: "student" }}
+          header="Welcome"
+          subheader="Please enter your class code."
+          initialValues={{ classId: "" }}
+          onSubmit={({ classId }) => {
+            navigate(generatePath(paths.login.student.class._, { classId }))
+          }}
+        >
+          <form.TextField
+            name="classId"
+            placeholder="Access code"
+            helperText="Enter your access code"
+            schema={classIdSchema}
+            required
+          />
+          <Typography variant="body2" fontWeight="bold">
+            Forgotten your login details? Please check with your teacher.
+          </Typography>
+          <Stack alignItems="end">
+            <form.SubmitButton endIcon={<ChevronRightIcon />}>
+              Next
+            </form.SubmitButton>
+          </Stack>
+        </BaseForm>
+      )}
+    </>
   )
 }
 
