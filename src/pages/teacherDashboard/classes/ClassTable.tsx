e0126fccb6f9f05fb27842a3e218ad29e8fdbdd7
SKairinos <skairinos@gmail.com>
2024-08-27 13:19:44 +0000

    simplify

--- src/pages/teacherDashboard/classes/ClassTable.tsx
+++ src/pages/teacherDashboard/classes/ClassTable.tsx
@@ -15,61 +15,59 @@ export interface ClassTableProps {
   authUser: SchoolTeacherUser<RetrieveUserResult>
 }
 
-const ClassTable: FC<ClassTableProps> = ({ authUser }) => {
-  return (
-    <>
-      <Typography align="center" variant="h4">
-        Your classes
-      </Typography>
-      <Typography>
-        Below is a list of all the classes in your school, including classes of
-        other teachers. You can add a class or edit your existing classes. You
-        can also accept or deny requests from independent students wanting to
-        join one of your classes.
-      </Typography>
-      <TablePagination useLazyListQuery={useLazyListClassesQuery}>
-        {classes => (
-          <tables.Table
-            titles={
-              authUser.teacher.is_admin
-                ? ["Class name", "Access code", "Teacher", "Action"]
-                : ["Class name", "Access code", "Action"]
-            }
-          >
-            {classes.map(klass => (
-              <tables.Body key={`klass-${klass.id}`}>
-                <tables.Cell>{klass.name}</tables.Cell>
-                <tables.Cell justifyContent="space-between" alignItems="center">
-                  {klass.id}
-                  <CopyIconButton content={klass.id} />
+const ClassTable: FC<ClassTableProps> = ({ authUser }) => (
+  <>
+    <Typography align="center" variant="h4">
+      Your classes
+    </Typography>
+    <Typography>
+      Below is a list of all the classes in your school, including classes of
+      other teachers. You can add a class or edit your existing classes. You can
+      also accept or deny requests from independent students wanting to join one
+      of your classes.
+    </Typography>
+    <TablePagination useLazyListQuery={useLazyListClassesQuery}>
+      {classes => (
+        <tables.Table
+          titles={
+            authUser.teacher.is_admin
+              ? ["Class name", "Access code", "Teacher", "Action"]
+              : ["Class name", "Access code", "Action"]
+          }
+        >
+          {classes.map(klass => (
+            <tables.Body key={`klass-${klass.id}`}>
+              <tables.Cell>{klass.name}</tables.Cell>
+              <tables.Cell justifyContent="space-between" alignItems="center">
+                {klass.id}
+                <CopyIconButton content={klass.id} />
+              </tables.Cell>
+              {authUser.teacher.is_admin && (
+                <tables.Cell>
+                  {klass.teacher.id === authUser.teacher.id
+                    ? "You"
+                    : `${klass.teacher.user.first_name} ${klass.teacher.user.last_name}`}
                 </tables.Cell>
-                {authUser.teacher.is_admin && (
-                  <tables.Cell>
-                    {klass.teacher.id === authUser.teacher.id
-                      ? "You"
-                      : `${klass.teacher.user.first_name} ${klass.teacher.user.last_name}`}
-                  </tables.Cell>
-                )}
-                <tables.Cell justifyContent="center">
-                  <LinkButton
-                    to={generatePath(
-                      paths.teacher.dashboard.tab.classes.class._,
-                      {
-                        classId: klass.id,
-                      },
-                    )}
-                    endIcon={<CreateIcon />}
-                  >
-                    Edit details
-                  </LinkButton>
-                </tables.Cell>
-              </tables.Body>
-            ))}
-          </tables.Table>
-        )}
-      </TablePagination>
-    </>
-  )
-}
+              )}
+              <tables.Cell justifyContent="center">
+                <LinkButton
+                  to={generatePath(
+                    paths.teacher.dashboard.tab.classes.class._,
+                    {
+                      classId: klass.id,
+                    },
+                  )}
+                  endIcon={<CreateIcon />}
+                >
+                  Edit details
+                </LinkButton>
+              </tables.Cell>
+            </tables.Body>
+          ))}
+        </tables.Table>
+      )}
+    </TablePagination>
+  </>
+)
 
 export default ClassTable
