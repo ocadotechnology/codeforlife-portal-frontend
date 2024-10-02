2bc3878fa0a4de7123f2380c1c9ef613caea117c
SKairinos <skairinos@gmail.com>
2024-10-02 15:34:46 +0000

    add remaining components

--- src/pages/homeLearning/HomeLearning.tsx
+++ src/pages/homeLearning/HomeLearning.tsx
@@ -1,8 +1,15 @@
 import * as pages from "codeforlife/components/page"
 import { type FC } from "react"
+import { LinkButton } from "codeforlife/components/router"
+import { Send as SendIcon } from "@mui/icons-material"
 
 import AboutRR from "./AboutRR"
+import Advanced from "./Advanced"
+import Beginner from "./Beginner"
 import HomeLearningHeroImage from "../../images/home_learning_hero_hexagon.png"
+import Intermediate from "./Intermediate"
+import { LINK_IDEAS_BOX } from "../../app/env"
+import { paths } from "../../routes"
 
 export interface HomeLearningProps {}
 
@@ -16,6 +23,21 @@ const HomeLearning: FC<HomeLearningProps> = () => {
       />
       <pages.Section>
         <AboutRR />
+      </pages.Section>
+      <Beginner />
+      <Intermediate />
+      <Advanced />
+      <pages.Section className="flex-end-x">
+        <LinkButton to={paths.register._}>Register now</LinkButton>
+      </pages.Section>
+      <pages.Section
+        boxProps={{ bgcolor: "tertiary.main" }}
+        className="flex-center-x"
+        sx={{ paddingY: theme => theme.spacing(2) }}
+      >
+        <LinkButton endIcon={<SendIcon />} to={LINK_IDEAS_BOX} target="_blank">
+          Let us know your feedback
+        </LinkButton>
       </pages.Section>
     </pages.Page>
   )
