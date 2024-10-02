5ba5ad91fbe2d3684002779735f4e1f47b3cc6a9
SKairinos <skairinos@gmail.com>
2024-10-02 10:38:27 +0000

    initial page and route

---
+++ src/pages/teacherDashboard/classes/releaseStudents/ReleaseStudents.tsx
@@ -0,0 +1,9 @@
+import { type FC } from "react"
+
+export interface ReleaseStudentsProps {}
+
+const ReleaseStudents: FC<ReleaseStudentsProps> = () => {
+  return <></>
+}
+
+export default ReleaseStudents
